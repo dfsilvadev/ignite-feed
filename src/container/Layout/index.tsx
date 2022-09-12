cb03d5c3cee7b1afe1a60baad614a7a56df08c43
Daniel Silva <danielsilva@iMac-de-Daniel.local>
2022-09-12 18:06:30 -0300

    feat: add sidebar component

---
+++ src/container/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../../components/Header";
+import { Sidebar } from "../Sidebar";
+
+import styles from "./styles.module.css";
+
+export function Layout() {
+  return (
+    <main>
+      <Header />
+      <div className={styles.container}>
+        <Sidebar />
+        <div>feed</div>
+      </div>
+    </main>
+  );
+}
