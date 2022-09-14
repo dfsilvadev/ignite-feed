6af9e0f19e8fc3cafea2f19ca6369ae4e1e2a04c
Daniel Silva <danielsilva@iMac-de-Daniel.local>
2022-09-14 10:21:33 -0300

    feat: add comments form

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -39,6 +39,15 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles["comment-form"]}>
+        <strong>Deixe o seu feedback</strong>
+        <textarea placeholder="Deixe o seu feedback" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
