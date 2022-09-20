e6bebf41494742ff728efcf9365d07295472d5da
Daniel Silva <dfsilva.dxp@gmail.com>
2022-09-20 16:39:38 -0300

    feat: add data mock

---
+++ src/utils/mock.ts
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://",
+      name: "Jhon Doe",
+      role: "Front-end Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+        url: "https://",
+      },
+    ],
+    hashtags: ["#hashone", "#hashtwo", "#hashthree"],
+    publishedAt: new Date("2022-09-03 20:45:16"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://",
+      name: "Jhoanna Doe",
+      role: "UI/UX Design",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+        url: "https://",
+      },
+    ],
+    hashtags: ["#hashfour", "#hashfive", "#hashsix"],
+    publishedAt: new Date("2022-08-22 10:26:38"),
+  },
+];
