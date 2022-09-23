953df6e5aace0441ec40470413cc6a12977a49e1
Daniel Silva <dfsilva.dxp@gmail.com>
2022-09-23 19:13:59 -0300

    feat: add comment mock

---
+++ src/utils/mockComments.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 100,
+    postId: 10,
+    content: {
+      author: {
+        avatarUrl: "https://i.pravatar.cc/150?img=6",
+        name: "Lee Patrick",
+      },
+      text: "Show!",
+    },
+    publishedAt: new Date("2022-09-04 12:45:16"),
+  },
+  {
+    id: 101,
+    postId: 10,
+    content: {
+      author: {
+        avatarUrl: "https://i.pravatar.cc/150?img=9",
+        name: "Lina Lins",
+      },
+      text: "Parabéns, John.",
+    },
+    publishedAt: new Date("2022-09-22 17:06:06"),
+  },
+  {
+    id: 10,
+    postId: 20,
+    content: {
+      author: {
+        avatarUrl: "https://i.pravatar.cc/150?img=9",
+        name: "Lina Lins",
+      },
+      text: "Adorei o projeto...",
+    },
+    publishedAt: new Date("2022-09-22 17:06:06"),
+  },
+];
